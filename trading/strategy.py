38ee02c11493c3fec9d173cc283e09b7e71520e1
Callum Abbott <callum.abbott@outlook.com>
2020-11-04 15:15:26 +0000

    random() algorithm implemented :sparkle:

--- trading/strategy.py
+++ trading/strategy.py
@@ -1,6 +1,7 @@
 # Functions to implement our trading strategy.
 import numpy as np
-import process as proc
+from trading import process as proc
+from trading import data as data
 
 def random(stock_prices, period=7, amount=5000, fees=20, ledger='ledger_random.txt'):
     '''
@@ -18,4 +19,23 @@ def random(stock_prices, period=7, amount=5000, fees=20, ledger='ledger_random.t
 
     Output: None
     '''
-    pass
+    # Number of stock to simulate
+    N = stock_prices.shape[1] 
+    # Create day 0 portfolio
+    portfolio = proc.create_portfolio(np.ones(N)*amount, stock_prices, fees, ledger)
+
+    # Determine number of actions and which days they occur
+    action_freq = int(np.floor(stock_prices.shape[0] / period) - 1)
+    action_days = [period*i for i in range(1, action_freq+1)]
+
+    # Loop over all actions
+    for i in range(action_freq):
+        # Loop over action for each stock
+        for j in range(N):
+            # Randomly sample action from discrete unif dist.
+            action = np.random.choice([0, 1, 2])
+
+            if action == 1: # Buying
+                proc.buy(action_days[i], j, amount, stock_prices, fees, portfolio, ledger)
+            elif action == 2: # Selling
+                proc.sell(action_days[i], j, stock_prices, fees, portfolio, ledger)
