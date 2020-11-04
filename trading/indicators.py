443139045fc201467c452d78c08b322a49b0c45b
Callum Abbott <callum.abbott@outlook.com>
2020-11-04 16:18:48 +0000

    Fixed some edge case bugs :bug:

--- trading/indicators.py
+++ trading/indicators.py
@@ -23,7 +23,7 @@ def moving_average(stock_price, n=7, weights=[]):
     ma = np.zeros(shape=(n_eff, ))
 
     if n > n_eff:
-        print(f'Warning: the first {n_eff} elements of the ma array returned are a' + \
+        print(f'Warning: The first {n_eff} elements of the ma array returned are a ' + \
                 f'{n_eff}-day moving average and not a {n}-day moving average')
 
     # Unweighted moving average
@@ -72,24 +72,43 @@ def oscillator(stock_price, n=7, osc_type='stochastic'):
             # Compute ingredients for osc
             delta = rev_stock_price[i:i+n][0] - low_price
             delta_max = high_price - low_price
-            # Return stochastic oscillator
-            osc[i] = delta / delta_max
+            # Handling division by 0 errors
+            if delta == 0 and delta_max == 0:
+                osc[i] = 0
+            elif delta != 0 and delta_max == 0:
+                osc[i] = 1
+            elif delta == 0 and delta_max != 0:
+                osc[i] = 0
+            elif delta != 0 and delta_max != 0:
+                # Return stochastic oscillator
+                osc[i] = delta / delta_max
         return osc[::-1]
     
     elif osc_type == 'RSI':
         price_diffs_arr = np.zeros(shape = (n_eff, n))
         # Get price differences over past n days
         for i in range(n_eff):
-            price_diffs_arr[i, :] = rev_stock_price[i:i+n] - rev_stock_price[i+1:i+1+n]
-            # Find +ve and -ve price diffs and return RSI
-            plus_idxs = np.where(price_diffs_arr[i, :] > 0)[0] # Do not include 0 in RSI calculation
-            neg_idxs = np.where(price_diffs_arr[i, :] < 0)[0]
-            plus_avg = np.mean(price_diffs_arr[i, :][plus_idxs])
-            neg_avg = np.abs(np.mean(price_diffs_arr[i, :][neg_idxs]))
-            RS = plus_avg / neg_avg
-            RSI = -1/(1 + RS) + 1
-            osc[i] = RSI
+            # Set osc to 0 for when we don't have enough data to calculate RSI
+            if rev_stock_price[i:i+n].size != rev_stock_price[i+1:i+1+n].size:
+                osc[i] = 0
+            else:
+                # Get n-day price differences
+                price_diffs_arr[i, :] = rev_stock_price[i:i+n] - rev_stock_price[i+1:i+1+n]
+                # Find +ve and -ve price diffs and return RSI
+                plus_idxs = np.where(price_diffs_arr[i, :] > 0)[0] # Do not include 0 in RSI calculation
+                neg_idxs = np.where(price_diffs_arr[i, :] < 0)[0]
+                plus_avg = np.mean(price_diffs_arr[i, :][plus_idxs])
+                neg_avg = np.abs(np.mean(price_diffs_arr[i, :][neg_idxs]))
+                # Handle edge case where RS blows up so RSI tends to 1.0!
+                if neg_avg == 0:
+                    osc[i] = 1.0
+                else:
+                    RS = plus_avg / neg_avg
+                    RSI = -1/(1 + RS) + 1
+                    osc[i] = RSI
         return osc[::-1]
+
+
 
 
 
